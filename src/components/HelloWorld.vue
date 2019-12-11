f88225ce8de3f5398027c78db0c846e019daebf4
Your Name <you@example.com>
2019-12-12 01:53:16 +0800

    'asd'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,64 @@
 /* eslint-disable no-console */
 <template>
   <div class="hello">
-   <input type="file" id="file" accept=".txt" @change=uploadfile />
-   <div id="result"> </div>
-      <template v-for="(item,index) in hanzi">
-        <ruby :key="index">{{item}}<rt>{{pinyin[index]}}</rt></ruby>
-      </template>
+    <button @click="tab = !tab">点击切换</button>
+    <div id="top" v-if="tab">
+      <input type="file" id="file" accept=".txt" @change="uploadfile" />
+    </div>
+    <div v-else>
+      <textarea
+        v-model="str"
+        ref="text"
+        @blur="fn"
+        name=""
+        id="text"
+      ></textarea>
+    </div>
+    <div id="result"></div>
+    <template v-for="(item, index) in hanzi">
+      <ruby :key="index"
+        >{{ item }}<rt>{{ pinyin[index] }}</rt></ruby
+      >
+    </template>
+    <a href="#top">回到顶部</a>
   </div>
 </template>
 
 <script>
 var pinyin = require("pinyin");
- var han = /^[\u4e00-\u9fa5]+$/;
-function isChinese(temp){
-  if (han.test(temp)){
-     return true
+var han = /^[\u4e00-\u9fa5]+$/;
+function isChinese(temp) {
+  if (han.test(temp)) {
+    return true;
   }
 }
 export default {
   name: "HelloWorld",
   data() {
     return {
-      hanzi:[],
-      pinyin:[],
-      str:''
+      hanzi: [],
+      pinyin: [],
+      str: "",
+      tab: false
     };
   },
   props: {
     msg: String
   },
-  mounted(){
+  mounted() {
     // this.fn()
   },
   methods: {
     uploadfile() {
       let reads = new FileReader();
-      var file = document.getElementById('file').files[0];
-      reads.readAsText(file, 'utf-8');
+      var file = document.getElementById("file").files[0];
+      reads.readAsText(file, "utf-8");
       console.log(reads);
-      var that = this
-      reads.onload = function (e) {
-          // document.getElementById('result').innerText = e.target.result
-          that.str = e.target.result
-          that.fn()
+      var that = this;
+      reads.onload = function(e) {
+        // document.getElementById('result').innerText = e.target.result
+        that.str = e.target.result;
+        that.fn();
       };
       // reads.onloadstart = function(e) {
       //     console.log('onloadstart ---> ', e)
@@ -54,30 +70,30 @@ export default {
       //     console.log('onprogress ---> ', e)
       // }
     },
-    fn(){
+    fn() {
       // var arr =[]
-      var str = this.str
+      var str = this.str;
       // arr = Array.from(str)
-      str = str.replace(/\s*/g,"");
-      var arr2 = []
-      var arr3 = []
+      str = str.replace(/\s*/g, "");
+      var arr2 = [];
+      var arr3 = [];
       for (let index = 0; index < str.length; index++) {
         const item = str[index];
-        if(isChinese(item)){
-          arr2.push(item)
-        }else{
-          arr3.push({index,item})
+        if (isChinese(item)) {
+          arr2.push(item);
+        } else {
+          arr3.push({ index, item });
         }
       }
-      arr2 = arr2.join('')
+      arr2 = arr2.join("");
       // console.log(arr3)
-      this.hanzi = Array.from(arr2)
-      this.pinyin = pinyin(arr2)
+      this.hanzi = Array.from(arr2);
+      this.pinyin = pinyin(arr2);
       // console.log(this.hanzi)
       for (let i = 0; i < arr3.length; i++) {
-        const item = arr3[i]
-        this.hanzi.splice(item.index,0,item.item)
-        this.pinyin.splice(item.index,0,item.item)
+        const item = arr3[i];
+        this.hanzi.splice(item.index, 0, item.item);
+        this.pinyin.splice(item.index, 0, item.item);
       }
     }
   }
@@ -92,11 +108,11 @@ li {
   border: 1px solid;
   cursor: pointer;
 }
-rt{
+rt {
   color: red;
 }
-ruby{
-  margin:2px;
+ruby {
+  margin: 2px;
   line-height: 60px;
 }
 </style>
